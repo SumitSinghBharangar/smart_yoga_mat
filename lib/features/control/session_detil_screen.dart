93818fca1cac7f7bc1f5524eb7c6b6e227c0d349
Sumit <bharangarsinghsumit@gmail.com>
2025-05-22 00:41:08 +0530

    pushed

---
+++ lib/features/control/session_detil_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SessionDetilScreen extends StatefulWidget {
+  const SessionDetilScreen({super.key});
+
+  @override
+  State<SessionDetilScreen> createState() => _SessionDetilScreenState();
+}
+
+class _SessionDetilScreenState extends State<SessionDetilScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/features/music_&_sound/music_sound_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MusicSoundScreen extends StatefulWidget {
+  const MusicSoundScreen({super.key});
+
+  @override
+  State<MusicSoundScreen> createState() => _MusicSoundScreenState();
+}
+
+class _MusicSoundScreenState extends State<MusicSoundScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
